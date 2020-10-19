5be52e97e0d3d70855b898eea60abf306aff5127
Lysander <lysander.hans@hotmail.com>
2020-10-19 10:39:45 +0200

    📦 Published spatie/laravel-medialibrary

--- config/app.php
+++ config/app.php
@@ -152,6 +152,7 @@ return [
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
         Illuminate\Hashing\HashServiceProvider::class,
         Illuminate\Mail\MailServiceProvider::class,
+        \Spatie\MediaLibrary\MediaLibraryServiceProvider::class,
         Illuminate\Notifications\NotificationServiceProvider::class,
         Illuminate\Pagination\PaginationServiceProvider::class,
         Illuminate\Pipeline\PipelineServiceProvider::class,
